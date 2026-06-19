c5d1b508fa0a10e07a03e8da4fe343b21d8789f6
SanskarLoganDev <devlogan70@gmail.com>
2026-06-19 12:31:00 -0400

    added songs and playbar to spotify project

--- Projects/spotify/script.js
+++ Projects/spotify/script.js
@@ -1,2 +1,9 @@
-console.log("Javascript time!!")
+console.log("Javascript time!!");
 
+async function main() {
+  let a = await fetch("http://127.0.0.1:5500/Projects/spotify/songs/");
+  let response = await a.text();
+  console.log(response)
+}
+
+main()
